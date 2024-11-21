26fc46bff9c868f4f260edb78740b068c15292b8
Eric Liu <ericyl.us@gmail.com>
2024-11-21 14:09:41 -0800

    Use `carbon-preprocess-svelte` for faster, optimized builds
    Signed-off-by: Eric Liu <ericyl.us@gmail.com>

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,6 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { optimizeCss } from 'carbon-preprocess-svelte';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+
+		// Optimize CSS from `carbon-components-svelte` when building for production.
+		optimizeCss()
+	],
+
+	// For even faster cold starts, exclude packages that `optimizeImports` will resolve.
+	optimizeDeps: {
+		exclude: ['carbon-components-svelte', 'carbon-icons-svelte']
+	}
 });
