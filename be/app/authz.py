48d04a1619675ddd815063802ee3edd07465f48b
Rakesh Jain <rakeshj@us.ibm.com>
2025-02-14 00:16:08 -0800

    add pluggable authorization

--- be/app/authz.py
+++ be/app/authz.py
@@ -6,7 +6,7 @@ class Authz(object):
 
     def __init__(self, request: Request, response: Response):
         #self.token = os.environ.get('TOKEN')
-        #self.user = request.session.get("user")
+        self.user = request.session.get("user")
         self.response = response
 
     def has_access(self, iceberg_table: str):
@@ -14,6 +14,8 @@ class Authz(object):
         handle your own authorization logic here, make sure to return 
         status.HTTP_403_FORBIDDEN if user doesn't have access to table data
         '''
+        if not self.user: #authentication is not enabled otherwise user would be in session
+            return True
         if False:
             self.response.status_code = status.HTTP_403_FORBIDDEN
         return True
