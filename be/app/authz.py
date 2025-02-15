037398fa8449412b7a489f6b19c23f89061680f0
Rakesh Jain <rakeshj@us.ibm.com>
2025-02-14 22:48:04 -0800

    minor change in authz

--- be/app/authz.py
+++ be/app/authz.py
@@ -4,17 +4,17 @@ from fastapi import Request, Response, status
 
 class Authz(object):
 
-    def __init__(self, request: Request, response: Response):
-        #self.token = os.environ.get('TOKEN')
-        self.user = request.session.get("user")
-        self.response = response
+    def __init__(self):
+        cat_url = os.environ.get('PYICEBERG_CATALOG__DEFAULT__URI')
 
-    def has_access(self, iceberg_table: str):
+
+    def has_access(self, request: Request, response: Response, table_id: str):
         '''
         handle your own authorization logic here, make sure to return 
         status.HTTP_403_FORBIDDEN if user doesn't have access to table data
         '''
-        if not self.user: #authentication is not enabled otherwise user would be in session
+        user = request.session.get("user")
+        if not user: #authentication is not enabled otherwise user would be in session
             return True
         if False:
             self.response.status_code = status.HTTP_403_FORBIDDEN
