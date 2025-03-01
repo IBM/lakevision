01d6fd08bd79592c02fedcb5269adebf138cdf37
Rakesh Jain <rakeshj@us.ibm.com>
2025-03-01 00:20:18 -0600

    fix nginx status

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@ fastapi run /app/be/app/api.py --port 8000 &
 #gunicorn -k uvicorn.workers.UvicornWorker api:app --bind 0.0.0.0:8000 --workers 2 --threads 32
 cd /app/fe
 npm run build
-node build &
-nginx -g 'daemon off;'
-#npm run dev -- --host&
+nginx -g 'daemon off;' &
+node build
 
 
